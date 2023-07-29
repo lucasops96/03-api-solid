e7dc7b3c9dc3da9f0d3596a01126d0ca5abb75ee
João Lucas <lucasbanana96@gmail.com>
2023-07-29 19:55:04 -0300

    authenticate with JWT

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['dev','test','production']).default('dev'),
-    PORT: z.coerce.number().default(3333)
+    JWT_SECRET: z.string(),
+    PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
@@ -14,5 +15,5 @@ if(_env.success === false){
 
     throw new Error('Invalid environment variables') 
 }
-
+ 
 export const env = _env.data
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -3,7 +3,10 @@ import { z } from 'zod'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
-export async function authenticate(request:FastifyRequest, reply:FastifyReply){
+export async function authenticate(
+    request:FastifyRequest, 
+    reply:FastifyReply
+){
     const authenticateBodySchema = z.object({
         email: z.string().email(),
         password: z.string().min(6),
@@ -14,7 +17,20 @@ export async function authenticate(request:FastifyRequest, reply:FastifyReply){
     try {
         const authenticateUseCase = makeAuthenticateUseCase()
 
-        await authenticateUseCase.execute({email,password})
+        const { user } = await authenticateUseCase.execute({email,password})
+
+        const token = await reply.jwtSign(
+            {},
+            {
+                sign:{
+                    sub: user.id,
+                },
+            },
+        )
+
+        return reply.status(200).send({
+            token,
+        })
     } catch (err) {
         if(err instanceof InvalidCredentialsError){
             return reply.status(400).send({message: err.message})
@@ -23,5 +39,5 @@ export async function authenticate(request:FastifyRequest, reply:FastifyReply){
         throw err
     }
 
-    return reply.status(200).send()
+    
 }
