36ef3ca1dfdbfce92bb09bc7320feda644a34131
João Lucas <lucasbanana96@gmail.com>
2023-07-31 16:17:41 -0300

    controllers gyms

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,9 +2,9 @@ import { FastifyInstance } from 'fastify';
 import { register } from './controllers/register';
 import { authenticate } from './controllers/authenticate';
 import { profile } from './controllers/profile';
-import { verifyJWT } from './middlewares/verify-jwt';
+import { verifyJWT } from '../../middlewares/verify-jwt';
 
-export async function appRoutes(app: FastifyInstance){
+export async function usersRoutes(app: FastifyInstance){
     app.post('/users', register)
     app.post('/sessions', authenticate)
 
